9c4019d1bd85e18ceaeac938f0ee95902d2275b8
Muna <mmhussein125@gmail.com>
2018-10-29 17:03:55 +0000

    Increasing/decreading temperature, power saving & min/max temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,4 +8,28 @@ describe("Thermostat", function() {
   it("should return 20 at the start", function() {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   })
+
+  it("should increase the temperature by 1", function() {
+    thermostat.increaseTemperature();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  })
+
+  it("should decrease the temperature by 1", function() {
+    thermostat.decreaseTemperature();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  })
+
+  it("should not drop below 10", function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.decreaseTemperature();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  })
+
+  it("should not rise above 25", function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.increaseTemperature();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(25);
+  })
 })
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,7 +1,33 @@
+'use strict';
+
 function Thermostat() {
   this.temperature = 20;
+  this.MIN_TEMPERATURE = 10
+  this.powerSaving = true
+  this.MAX_TEMPERATURE = 25
+
 }
 
 Thermostat.prototype.getCurrentTemperature = function() {
   return this.temperature;
+};
+
+Thermostat.prototype.increaseTemperature = function() {
+if (!this.isMaxTemperature()) {
+  this.temperature += 1;
+}
+};
+
+Thermostat.prototype.decreaseTemperature = function() {
+  if (!this.isMinTemperature()) {
+    this.temperature -= 1;
+  }
+};
+
+Thermostat.prototype.isMinTemperature = function() {
+   return this.temperature === this.MIN_TEMPERATURE
+};
+
+Thermostat.prototype.isMaxTemperature = function() {
+   return this.temperature === this.MAX_TEMPERATURE
 };
