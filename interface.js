22d0ad4401cfb113293791d981b7ceb832d7f44c
Todd Langford-Archer <toddpla@gmail.com>
2018-10-31 15:34:39 +0000

    linked up the interface

--- interface.js
+++ interface.js
@@ -1,9 +1,30 @@
+let updateState = function() {
+  $( "#temperature" ).text(thermostat.getCurrentTemperature())
+  $( "#power-saving-status" ).text(thermostat.powerSaving)
+  $( "#temperature" ).attr("class", thermostat.usage());
+}
+
 $( document ).ready(function() {
   thermostat = new Thermostat();
-  $( "#temperature" ).text(thermostat.getCurrentTemperature())
+  updateState()
 });
 
 $( "#temperature-up" ).click(function() {
   thermostat.increaseTemperature();
-  $( "#temperature" ).text(thermostat.getCurrentTemperature())
+  updateState()
+});
+
+$( "#temperature-down" ).click(function() {
+  thermostat.decreaseTemperature();
+  updateState()
+});
+
+$( "#temperature-reset" ).click(function() {
+  thermostat.reset();
+  updateState()
+});
+
+$( "#powersaving" ).click(function() {
+  thermostat.switchPS();
+  updateState()
 });
