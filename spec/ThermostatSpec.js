e60508d338e04cdc31157c30527e8656c195b2c6
Todd Langford-Archer <toddpla@gmail.com>
2018-10-31 14:19:16 +0000

    added thermostat functionality

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -26,10 +26,51 @@ describe("Thermostat", function() {
     expect(thermostat.getCurrentTemperature()).toEqual(10);
   })
 
-  it("should not rise above 25", function() {
+  it("should not rise above 25 if power saving mode on", function() {
     for (var i = 0; i < 11; i++) {
       thermostat.increaseTemperature();
     }
     expect(thermostat.getCurrentTemperature()).toEqual(25);
   })
+
+  it("should not rise above 32 if power saving mode off", function() {
+    thermostat.powerSaving = false;
+    for (var i = 0; i < 20; i++) {
+      thermostat.increaseTemperature();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(32);
+  })
+
+  it("should switch power saving mode from on to off", function() {
+    thermostat.switchPS();
+    expect(thermostat.powerSaving).toEqual(false);
+  })
+
+  it("should switch power saving mode from off to on", function() {
+    thermostat.powerSaving = false;
+    thermostat.switchPS();
+    expect(thermostat.powerSaving).toEqual(true);
+  })
+
+  it("should reset temperature to 20", function() {
+    thermostat.temperature = 30;
+    thermostat.reset();
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  })
+
+  it("current useage should be 'low-usage' < 18", function() {
+    thermostat.temperature = 15;
+    expect(thermostat.usage()).toEqual('low-usage');
+  })
+
+  it("current useage should be 'medium-usage' < 25", function() {
+    thermostat.temperature = 24;
+    expect(thermostat.usage()).toEqual('medium-usage');
+  })
+
+  it("current useage should be 'medium-usage' >= 25", function() {
+    thermostat.temperature = 25;
+    expect(thermostat.usage()).toEqual('high-usage');
+  })
+
 })
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -4,7 +4,8 @@ function Thermostat() {
   this.temperature = 20;
   this.MIN_TEMPERATURE = 10
   this.powerSaving = true
-  this.MAX_TEMPERATURE = 25
+  this.MAX_TEMP_POWER_SAVING = 25
+  this.MAX_TEMP = 32
 }
 
 Thermostat.prototype.getCurrentTemperature = function() {
@@ -28,5 +29,27 @@ Thermostat.prototype.isMinTemperature = function() {
 };
 
 Thermostat.prototype.isMaxTemperature = function() {
-   return this.temperature === this.MAX_TEMPERATURE
+  if (this.powerSaving) {
+    return this.temperature === this.MAX_TEMP_POWER_SAVING
+  } else {
+    return this.temperature === this.MAX_TEMP
+  }
 };
+
+Thermostat.prototype.switchPS = function() {
+  this.powerSaving = !this.powerSaving;
+}
+
+Thermostat.prototype.reset = function() {
+  this.temperature = 20;
+}
+
+Thermostat.prototype.usage = function() {
+  if (this.temperature < 18) {
+    return "low-usage";
+  } else if (this.temperature < 25) {
+    return "medium-usage";
+  } else {
+    return "high-usage";
+  }
+}
